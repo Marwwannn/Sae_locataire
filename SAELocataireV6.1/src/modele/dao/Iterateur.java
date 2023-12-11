f68c2c8dc765cbfc2d6e143f887bcc4e57fb9bd0
sirfou31 <sirfou2002@gmail.com>
2023-12-11 15:22:08 +0100

    v

--- SAELocataireV6.1/src/modele/dao/Iterateur.java
+++ SAELocataireV6.1/src/modele/dao/Iterateur.java
@@ -6,51 +6,51 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class Iterateur<T> implements Iterator<T> {
-    private ResultSet resultSet;
-    private DaoModele<T> daoModele;
-    private T currentInstance;
-    private boolean hasNext;
+	private ResultSet resultSet;
+	private DaoModele<T> daoModele;
+	private T currentInstance;
+	private boolean hasNext;
 
-    // Constructeur
-    public Iterateur(ResultSet resultSet, DaoModele<T> daoModele) {
-        this.resultSet = resultSet;
-        this.daoModele = daoModele;
-        moveToNext(); // Move to the first element in the constructor
-    }
+	// Constructeur
+	public Iterateur(ResultSet resultSet, DaoModele<T> daoModele) {
+		this.resultSet = resultSet;
+		this.daoModele = daoModele;
+		moveToNext(); // Move to the first element in the constructor
+	}
 
-    // Méthode hasNext
-    @Override
-    public boolean hasNext() {
-        return hasNext;
-    }
+	// Méthode hasNext
+	@Override
+	public boolean hasNext() {
+		return hasNext;
+	}
 
-    // Méthode next
-    @Override
-    public T next() {
-        if (!hasNext) {
-            throw new NoSuchElementException();
-        }
+	// Méthode next
+	@Override
+	public T next() {
+		if (!hasNext) {
+			throw new NoSuchElementException();
+		}//ok
 
-        T nextInstance = currentInstance; // Store the current instance
-        moveToNext(); // Move to the next element
+		T nextInstance = currentInstance; // Store the current instance
+		moveToNext(); // Move to the next element
 
-        return nextInstance;
-    }
+		return nextInstance;
+	}
 
-    // Méthode privée pour déplacer le curseur vers l'élément suivant
-    private void moveToNext() {
-        try {
-            // Try to move to the next element
-            hasNext = resultSet.next();
+	// Méthode privée pour déplacer le curseur vers l'élément suivant
+	private void moveToNext() {
+		try {
+			// Try to move to the next element
+			hasNext = resultSet.next();
 
-            // If there is a next element, create the corresponding instance using daoModele
-            if (hasNext) {
-                currentInstance = daoModele.creerInstance(resultSet);
-            }
-        } catch (SQLException e) {
-            // Handle SQLException, you may choose to throw a runtime exception or handle it differently
-            e.printStackTrace();
-            hasNext = false;
-        }
-    }
+			// If there is a next element, create the corresponding instance using daoModele
+			if (hasNext) {
+				currentInstance = daoModele.creerInstance(resultSet);
+			}
+		} catch (SQLException e) {
+			// Handle SQLException, you may choose to throw a runtime exception or handle it differently
+			e.printStackTrace();
+			hasNext = false;
+		}
+	}
 }
