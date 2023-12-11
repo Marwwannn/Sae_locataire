88bd7ddbf0739a03f4bc3bdffd1c9b00bd6ca26e
sirfou31 <sirfou2002@gmail.com>
2023-12-11 15:19:15 +0100

    maversion

--- SAELocataireV6.1/src/modele/dao/Dao.java
+++ SAELocataireV6.1/src/modele/dao/Dao.java
@@ -1,15 +1,16 @@
 package modele.dao;
 
 import java.sql.SQLException;
+import java.util.Collection;
 
 public interface Dao<T> {
-    java.util.Collection<T> findAll() throws SQLException;
+	Collection<T> findAll() throws SQLException;
 
-    T findById(String... id) throws SQLException;
+	T findById(String... id) throws SQLException;
 
-    void update(T donnee) throws SQLException;
+	void update(T donnee) throws SQLException;
 
-    void create(T donnee) throws SQLException;
+	void create(T donnee) throws SQLException;
 
-    void delete(T donnee) throws SQLException;
+	void delete(T donnee) throws SQLException;
 }
--- SAELocataireV6.1/src/modele/dao/DaoLocataire.java
+++ SAELocataireV6.1/src/modele/dao/DaoLocataire.java
@@ -10,8 +10,7 @@ public class DaoLocataire extends DaoModele<Locataire> implements Dao<Locataire>
 
 	@Override
 	public Collection<Locataire> findAll() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		return Dao.selectCreneau();
 	}
 
 	@Override
@@ -23,39 +22,39 @@ public class DaoLocataire extends DaoModele<Locataire> implements Dao<Locataire>
 	@Override
 	public void update(Locataire donnee) throws SQLException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void create(Locataire donnee) throws SQLException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void delete(Locataire donnee) throws SQLException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	 protected Locataire creerInstance(ResultSet curseur) throws SQLException {
-        Locataire locataire = null;
-        try {
-            locataire = new Locataire(
-                    curseur.getString("ID_LOCATAIRE"),
-                    curseur.getString("NOM"),
-                    curseur.getString("PRENOM"),
-                    curseur.getString("TELEPHONE"),
-                    curseur.getString("MAIL"),
-                    curseur.getString("ADRESSE"),
-                    curseur.getString("CODE_POSTAL")
-            );
-        } catch (SQLException e) {
-            // Handle the exception appropriately, e.g., log or throw a custom exception
-            e.printStackTrace();
-        }
-        return locataire;
-    }
+	protected Locataire creerInstance(ResultSet curseur) throws SQLException {
+		Locataire locataire = null;
+		try {
+			locataire = new Locataire(
+					curseur.getString("ID_LOCATAIRE"),
+					curseur.getString("NOM"),
+					curseur.getString("PRENOM"),
+					curseur.getString("TELEPHONE"),
+					curseur.getString("MAIL"),
+					curseur.getString("ADRESSE"),
+					curseur.getString("CODE_POSTAL")
+					);
+		} catch (SQLException e) {
+			// Handle the exception appropriately, e.g., log or throw a custom exception
+			e.printStackTrace();
+		}
+		return locataire;
+	}
 
 }
